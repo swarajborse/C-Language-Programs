e69d2c14d0d5bdfb1b2afe9d87c5088cd778ee52
Gnana1607 <gnanapradhyumna@gmail.com>
2025-10-30 18:21:37 +0530

    Added C program to check if a matrix is symmetric or not
    This PR adds a C program that checks whether a given square matrix is symmetric or not.
    It compares each element with its transpose and prints the result.
    This contribution is made for Hacktoberfest 2025.

---
+++ check_symmetric_matrix.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, flag = 1;
+
+    printf("Enter the order of the square matrix: ");
+    scanf("%d", &n);
+    int matrix[n][n];
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 0)
+            break;
+    }
+    if (flag)
+        printf("The matrix is symmetric.\n");
+    else
+        printf("The matrix is not symmetric.\n");
+    return 0;
+}
